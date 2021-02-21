2e6c7abcaf26744a84ab1199cf682ed676f95434
openfsm <openfsm@163.com>
2021-02-21 08:46:06 +0800

    code review

--- spec.go
+++ spec.go
@@ -3,35 +3,35 @@ package spec
 import "github.com/open-fsm/spec/proto"
 
 var (
-	v0o1,v1o1,v1o2,v1o3,v1o4,v1o5,v1o6,v1o7,v2o0,v2o2,v2o4,v2o5,v2o6,v2o7,v2o8,v3o2,v2o1,v2o3,v3o3,
-	v3o4,v4o3,v4o4,v4o5,v4o6,v4o1,v4o2 proto.ViewStamp
+	V0o1,V1o1,V1o2,V1o3,V1o4,V1o5,V1o6,V1o7,V2o0,V2o2,V2o4,V2o5,V2o6,V2o7,V2o8,V3o2,V2o1,V2o3,V3o3,
+	V3o4,V4o3,V4o4,V4o5,V4o6,V4o1,V4o2 proto.ViewStamp
 )
 
 func InitViewStampCase() {
-	v0o1 = proto.ViewStamp{ViewNum: 0, OpNum: 1}
-	v1o1 = proto.ViewStamp{ViewNum: 1, OpNum: 1}
-	v1o2 = proto.ViewStamp{ViewNum: 1, OpNum: 2}
-	v1o3 = proto.ViewStamp{ViewNum: 1, OpNum: 3}
-	v1o4 = proto.ViewStamp{ViewNum: 1, OpNum: 4}
-	v1o5 = proto.ViewStamp{ViewNum: 1, OpNum: 5}
-	v1o6 = proto.ViewStamp{ViewNum: 1, OpNum: 6}
-	v1o7 = proto.ViewStamp{ViewNum: 1, OpNum: 7}
-	v2o0 = proto.ViewStamp{ViewNum: 2, OpNum: 0}
-	v2o2 = proto.ViewStamp{ViewNum: 2, OpNum: 2}
-	v2o4 = proto.ViewStamp{ViewNum: 2, OpNum: 4}
-	v2o5 = proto.ViewStamp{ViewNum: 2, OpNum: 5}
-	v2o6 = proto.ViewStamp{ViewNum: 2, OpNum: 6}
-	v2o7 = proto.ViewStamp{ViewNum: 2, OpNum: 7}
-	v2o8 = proto.ViewStamp{ViewNum: 2, OpNum: 8}
-	v3o2 = proto.ViewStamp{ViewNum: 3, OpNum: 2}
-	v3o4 = proto.ViewStamp{ViewNum: 3, OpNum: 4}
-	v2o1 = proto.ViewStamp{ViewNum: 2, OpNum: 1}
-	v2o3 = proto.ViewStamp{ViewNum: 2, OpNum: 3}
-	v3o3 = proto.ViewStamp{ViewNum: 3, OpNum: 3}
-	v4o3 = proto.ViewStamp{ViewNum: 4, OpNum: 3}
-	v4o4 = proto.ViewStamp{ViewNum: 4, OpNum: 4}
-	v4o5 = proto.ViewStamp{ViewNum: 4, OpNum: 5}
-	v4o6 = proto.ViewStamp{ViewNum: 4, OpNum: 6}
-	v4o1 = proto.ViewStamp{ViewNum: 4, OpNum: 1}
-	v4o2 = proto.ViewStamp{ViewNum: 4, OpNum: 2}
+	V0o1 = proto.ViewStamp{ViewNum: 0, OpNum: 1}
+	V1o1 = proto.ViewStamp{ViewNum: 1, OpNum: 1}
+	V1o2 = proto.ViewStamp{ViewNum: 1, OpNum: 2}
+	V1o3 = proto.ViewStamp{ViewNum: 1, OpNum: 3}
+	V1o4 = proto.ViewStamp{ViewNum: 1, OpNum: 4}
+	V1o5 = proto.ViewStamp{ViewNum: 1, OpNum: 5}
+	V1o6 = proto.ViewStamp{ViewNum: 1, OpNum: 6}
+	V1o7 = proto.ViewStamp{ViewNum: 1, OpNum: 7}
+	V2o0 = proto.ViewStamp{ViewNum: 2, OpNum: 0}
+	V2o2 = proto.ViewStamp{ViewNum: 2, OpNum: 2}
+	V2o4 = proto.ViewStamp{ViewNum: 2, OpNum: 4}
+	V2o5 = proto.ViewStamp{ViewNum: 2, OpNum: 5}
+	V2o6 = proto.ViewStamp{ViewNum: 2, OpNum: 6}
+	V2o7 = proto.ViewStamp{ViewNum: 2, OpNum: 7}
+	V2o8 = proto.ViewStamp{ViewNum: 2, OpNum: 8}
+	V3o2 = proto.ViewStamp{ViewNum: 3, OpNum: 2}
+	V3o4 = proto.ViewStamp{ViewNum: 3, OpNum: 4}
+	V2o1 = proto.ViewStamp{ViewNum: 2, OpNum: 1}
+	V2o3 = proto.ViewStamp{ViewNum: 2, OpNum: 3}
+	V3o3 = proto.ViewStamp{ViewNum: 3, OpNum: 3}
+	V4o3 = proto.ViewStamp{ViewNum: 4, OpNum: 3}
+	V4o4 = proto.ViewStamp{ViewNum: 4, OpNum: 4}
+	V4o5 = proto.ViewStamp{ViewNum: 4, OpNum: 5}
+	V4o6 = proto.ViewStamp{ViewNum: 4, OpNum: 6}
+	V4o1 = proto.ViewStamp{ViewNum: 4, OpNum: 1}
+	V4o2 = proto.ViewStamp{ViewNum: 4, OpNum: 2}
 }
